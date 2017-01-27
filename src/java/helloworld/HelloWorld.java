a8d94e1b31dc2e15e45fbdb0b42a4409d14c36c7
Ramzi Mukhar <rmukhar@ata-llc.com>
2017-01-27 16:18:13 -0500

    Hello World with ATA HQ.

--- src/java/helloworld/HelloWorld.java
+++ src/java/helloworld/HelloWorld.java
@@ -1,10 +1,9 @@
 package helloworld;
 
-public class HelloWorld {
-
+public class HelloWorld 
+{ 
     public static void main(String args[])
     {
         System.out.println("Hello World FROM ATA HQ!");
-    }
-    
+    }  
 }
